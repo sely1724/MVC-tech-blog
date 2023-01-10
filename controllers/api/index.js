dfe906a7ec456bd5f182ee50992f2f074ad6a973
Sylvia Ely <sylvianne9417@gmail.com>
2023-01-10 15:55:43 -0800

    think user route created? used inspiration from class activity 16

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+//login route
+const router = require("express").Router();
+
+const userRoutes = require("./userRoutes");
+
+router.use("/users", userRoutes);
+
+module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,22 +1,62 @@
 const router = require("express").Router();
 const { Users } = require("../../models/");
-const bcrypt = require("bcrypt");
+//const bcrypt = require("bcrypt");
 
 //create new user
-
 router.post("/", async (req, res) => {
-  Users.create({
-    username: req.body.username,
-    password: req.body.password,
-  });
   try {
-    (newUser) => {
-      res.json(newUser);
-    };
+    const dbUserData = await Users.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    // Set up sessions with a 'loggedIn' variable set to `true`
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      res.status(200).json(dbUserData);
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const dbUserData = await Users.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!dbUserData) {
+      res.status(400).json({
+        message: "Email not found.  Please re-enter or create a new account",
+      });
+      return;
+    }
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again!" });
+      return;
+    }
+
+    // Once the user successfully logs in, set up the sessions variable 'loggedIn'
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
