38efcfc1c25ad6c4978fb2ac2b00dcb57fc138eb
Sylvia Ely <sylvianne9417@gmail.com>
2023-01-10 16:13:43 -0800

    thinking thru route set up

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -3,3 +3,22 @@
 const express = require("express");
 const router = express.Router();
 const { Users, BlogPosts } = require("../../models");
+
+// get individual blog post - INCLUDES ALL COMMENTS.
+// /:id
+
+// option to add comments.
+// post /:id
+
+// get dashboard of blog posts - ALL INDIVIDUAL POSTS
+// /dashboard
+
+// get dashboard - unique post - includes comments
+// /dashboard/:id
+
+// create new post
+// /dashboard/new-post
+
+// click thru on dashboard?
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,9 @@
 const router = require("express").Router();
 
 const userRoutes = require("./userRoutes");
+const blogRoutes = require("./blogPostRoutes");
 
 router.use("/users", userRoutes);
+router.use("/blog", blogRoutes);
 
 module.exports = router;
