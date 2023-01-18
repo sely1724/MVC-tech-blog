f811aa44f1a0adcec0b003e8e771029d541cc489
Sylvia Ely <sylvianne9417@gmail.com>
2023-01-17 17:12:33 -0800

    FINALLY got put method to work

--- controllers/api/dashboard.js
+++ controllers/api/dashboard.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const withAuth = require("../../utils/withAuth");
-const { BlogPosts, Users } = require("../../models");
+const { BlogPosts } = require("../../models");
 
 router.post("/post", withAuth, async (req, res) => {
   try {
@@ -16,49 +16,26 @@ router.post("/post", withAuth, async (req, res) => {
   }
 });
 
-// get single personal post (shows delete/update button)
-// router.get("/post/:id", async (req, res) => {
-//   try {
-//     const dbSinglePost = await BlogPosts.findByPk(req.params.id, {
-//       where: {
-//         id: req.params.id,
-//       },
-//       include: [
-//         {
-//           model: Users,
-//           attributes: ["id", "username"],
-//         },
-//       ], //eventually include COMMENTS
-//     });
-//     console.log(dbSinglePost);
-//     const singlePost = dbSinglePost.get({ plain: true });
-//     //     // Send over the 'loggedIn' session variable to the 'homepage' template
-//     res.render("personal-post", {
-//       blog: singlePost,
-//       loggedIn: req.session.loggedIn,
-//       //       // loggedIn: req.session.loggedIn,
-//       //       // pass thru comments too?
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
+router.put("/:id", async (req, res) => {
+  try {
+    const updatePost = await BlogPosts.update(
+      {
+        title: req.body.title,
+        notes: req.body.notes,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
 
-// router.put("/:id", async (req, res) => {
-//   try {
-//     const updatePost = await BlogPosts.update(req.body, {
-//       where: {
-//         id: req.params.id,
-//       },
-
-//       //how to finish update?????
-//     });
-//     res.status(200).json(updatePost);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+      //how to finish update?????
+    );
+    res.status(200).json(updatePost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // delete route by specific id.
 router.delete("/:id", async (req, res) => {
--- public/js/dashboardEdit.js
+++ public/js/dashboardEdit.js
@@ -14,6 +14,37 @@ const deleteFormHandler = async (event) => {
     alert("Failed.");
   }
 };
+
+const updateFormHandler = async (event) => {
+  event.preventDefault();
+  console.log("event listener works");
+  const id = document.querySelector(".update-btn").value;
+  const title = document.querySelector("#title-update").value.trim();
+  const notes = document.querySelector("#notes-update").value.trim();
+
+  console.log(title);
+  console.log(notes);
+  console.log(id);
+  // both print out correctly
+  //if (title && notes) {
+  const response = await fetch("/api/dashboard/" + id, {
+    method: "PUT",
+    body: JSON.stringify({ title, notes }),
+    headers: { "Content-Type": "application/json" },
+  });
+  if (response.ok) {
+    document.location.replace("/dashboard/");
+    // console.log(req.body.title);
+  } else {
+    ("failed to update");
+  }
+};
+//};
+
+document
+  .querySelector(".update-form")
+  .addEventListener("submit", updateFormHandler);
+
 document
   .querySelector(".delete-btn")
   .addEventListener("click", deleteFormHandler);
